e0783677cc504829cea7d4922b5f69631e94ae7d
Sergo <sergojadesu@gmail.com>
2023-06-18 00:46:18 +1000

    Created and configured admin

--- todolist/core/admin.py
+++ todolist/core/admin.py
@@ -1,3 +1,17 @@
+from core.models import User
 from django.contrib import admin
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    fields = (
+        'username', 'first_name', 'last_name',
+        'email', 'is_staff', 'is_active',
+        'date_joined', 'last_login'
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    readonly_fields = ('date_joined', 'last_login')
+    search_fields = ['username', 'first_name', 'last_name']
+
+
+admin.site.register(User, UserAdmin)
