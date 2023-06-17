0e9c5cc9620f1a97e5fc5ab0dd13452b498cf560
Sergo <sergojadesu@gmail.com>
2023-06-17 23:48:32 +1000

    Created db, user

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7xv50_vtf(___yw9iydou*(*#uat^r%eeys_#*9vhh8d(p=22k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
